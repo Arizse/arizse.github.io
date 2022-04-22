be27d742c106ea8bba545df07100809c067074ea
Arizse <51176190+Arizse@users.noreply.github.com>
2022-04-22 17:09:17 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+// Performign a task 
+function greet(name, lastname) {
+    "use strict";
+ console.log('hello ' + name + ' ' + lastname);
+}
+// Calculating a value
+function square(number) {
+    "use strict";
+   return number * number;
+}
+console.log(square(2));
